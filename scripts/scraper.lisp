42f15f8c2c41de2b465093a4d3919395208e58cb
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-09-28 18:53:07 +0800

    Rename variables

--- scripts/scraper.lisp
+++ scripts/scraper.lisp
@@ -1,7 +1,7 @@
 (ql:quickload :drakma)
 (ql:quickload :cl-json)
 
-(defparameter *result* (json:decode-json-from-string
+(defparameter *surahs* (json:decode-json-from-string
                         (flexi-streams:octets-to-string
                          (drakma:http-request "http://api.alquran.cloud/surah"))))
 
@@ -10,10 +10,10 @@
    (remove-if-not (lambda (x) (equal (car x) indicator))
                   alist)))
 
-(defparameter *data* (get-json-value *result* :data))
+(defparameter *surahs-data* (get-json-value *surahs* :data))
 
 (defparameter *ayahs* (mapcar (lambda (x) (get-json-value x :number-of-ayahs))
-                              *data*))
+                              *surahs-data*))
 
 (defun print-number-of-ayahs ()
   (format t "~{~a~^, ~}" *ayahs*))
