22e87ac422a7c5e46ba7eca0bbb685b00b9cd922
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-09-28 20:08:20 +0800

    Refactor

--- scripts/scraper.lisp
+++ scripts/scraper.lisp
@@ -1,25 +1,35 @@
 (ql:quickload :drakma)
 (ql:quickload :cl-json)
 
-(defparameter *surahs* (json:decode-json-from-string
-                        (flexi-streams:octets-to-string
-                         (drakma:http-request "http://api.alquran.cloud/surah"))))
+(defun json-request (url)
+  (json:decode-json-from-string
+   (flexi-streams:octets-to-string
+    (drakma:http-request url))))
 
 (defun get-json-value (alist indicator)
   (cdar
-   (remove-if-not (lambda (x) (equal (car x) indicator))
-                  alist)))
+   (remove-if-not  ; filter
+    (lambda (x) (equal (car x) indicator))
+    alist)))
 
-(defparameter *surahs-data* (get-json-value *surahs* :data))
+(defparameter *surahs*
+  (json-request "http://api.alquran.cloud/surah"))
 
-(defparameter *ayahs* (mapcar (lambda (x) (get-json-value x :number-of-ayahs))
-                              *surahs-data*))
+(defparameter *surahs-data*
+  (get-json-value *surahs* :data))
+
+(defparameter *ayahs*
+  (mapcar
+   (lambda (surah) (get-json-value surah :number-of-ayahs))
+   *surahs-data*))
 
 (defun print-number-of-ayahs ()
   (format t "~{~a~^, ~}" *ayahs*))
 
-(defparameter *english-names* (mapcar (lambda (x) (get-json-value x :english-name))
-                                      *data*))
+(defparameter *english-names*
+  (mapcar
+   (lambda (surah) (get-json-value surah :english-name))
+   *surahs-data*))
 
 (defun print-title-strings ()
   (format t "~:{<string name=\"title_~a\">~a</string>~%~}"
